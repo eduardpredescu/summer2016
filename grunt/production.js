5ae90ab3d940fc700643024fe5101c2345f7e784
Valentin Popescu <ppsc.valentin@gmail.com>
2016-04-12 11:41:00 +0300

    Updated production tasks

--- grunt/production.js
+++ grunt/production.js
@@ -1,4 +1,15 @@
 module.exports = {
+  less: {
+    production: {
+      options: {
+        compress: true,
+        sourceMap: false
+      },
+      files: {
+        '<%= path.static %>/css/main.css': '<%= path.src %>/less/main.less'
+      }
+    }
+  },
   postcss: {
     production: {
       src: '<%= path.static %>/css/main.css',
