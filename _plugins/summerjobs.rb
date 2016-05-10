3f430206a95b76685b6f06e705a97f67cb591ddb
Valentin Popescu <ppsc.valentin@gmail.com>
2016-05-10 17:06:20 +0300

    Excluded demo pages

--- _plugins/summerjobs.rb
+++ _plugins/summerjobs.rb
@@ -26,36 +26,42 @@ module Summerjobs
 
         # Companies
         if page.data['company']
+          if not page.data['demo']
+            company = page.data['company']
+            company['id'] = slug(company['name'])
+            company['jobs'] = {}
+            company['page'] = page
 
-          company = page.data['company']
-          company['id'] = slug(company['name'])
-          company['jobs'] = {}
-          company['page'] = page
+            page.data.merge!({
+              'layout' => 'company',
+              'company' => company
+            })
 
-          page.data.merge!({
-            'layout' => 'company',
-            'company' => company
-          })
-
-          @companies[company['id']] = company
+            @companies[company['id']] = company
+          else
+            page.data['layout'] = 'company'  
+          end
 
         # Jobs
         elsif page.data['job']
+          if not page.data['demo']
+          
+            job = page.data['job']
+            job['company'] = (company = getCompany(page))
+            job['id'] = company['id'] + '_' + slug(job['title'])
+            job['page'] = page
 
-          job = page.data['job']
-          job['company'] = (company = getCompany(page))
-          job['id'] = company['id'] + '_' + slug(job['title'])
-          job['page'] = page
+            company['jobs'][job['id']] = job
 
-          company['jobs'][job['id']] = job
-
-          page.data.merge!({
-            'layout' => 'job',
-            'job' => job,
-            'company' => company
-          })
-
-          @jobs[job['id']] = job
+            page.data.merge!({
+              'layout' => 'job',
+              'job' => job,
+              'company' => company
+            })
+            @jobs[job['id']] = job
+          else
+            page.data['layout'] = 'job'
+          end
 
         # Other pages
         else
