822611b00cdcc6da7d155cece85a77c286e01292
Valentin Popescu <ppsc.valentin@gmail.com>
2016-05-13 16:49:46 +0300

    minor update to summerjobs.rb

--- _plugins/summerjobs.rb
+++ _plugins/summerjobs.rb
@@ -24,52 +24,55 @@ module Summerjobs
         # page.data['dateCreated'] = dateCreated
         # page.data['dateModified'] = dateModified
 
-        # Companies
-        if page.data['company']
-          if not page.data['demo']
-            company = page.data['company']
-            company['id'] = slug(company['name'])
-            company['jobs'] = {}
-            company['page'] = page
-
-            page.data.merge!({
-              'layout' => 'company',
-              'company' => company
-            })
-
-            @companies[company['id']] = company
-          else
-            page.data['layout'] = 'company'  
-          end
-
-        # Jobs
-        elsif page.data['job']
-          if not page.data['demo']
+        if page.data
           
-            job = page.data['job']
-            job['company'] = (company = getCompany(page))
-            job['id'] = company['id'] + '_' + slug(job['title'])
-            job['page'] = page
+          # Companies
+          if page.data['company']
+            if not page.data['demo']
+              company = page.data['company']
+              company['id'] = slug(company['name'])
+              company['jobs'] = {}
+              company['page'] = page
 
-            company['jobs'][job['id']] = job
+              page.data.merge!({
+                'layout' => 'company',
+                'company' => company
+              })
 
-            page.data.merge!({
-              'layout' => 'job',
-              'job' => job,
-              'company' => company
-            })
-            @jobs[job['id']] = job
+              @companies[company['id']] = company
+            else
+              page.data['layout'] = 'company'  
+            end
+
+          # Jobs
+          elsif page.data['job']
+            if not page.data['demo']
+            
+              job = page.data['job']
+              job['company'] = (company = getCompany(page))
+              job['id'] = company['id'] + '_' + slug(job['title'])
+              job['page'] = page
+
+              company['jobs'][job['id']] = job
+
+              page.data.merge!({
+                'layout' => 'job',
+                'job' => job,
+                'company' => company
+              })
+              @jobs[job['id']] = job
+            else
+              page.data['layout'] = 'job'
+            end
+
+          # Other pages
           else
-            page.data['layout'] = 'job'
+
+            @otherPages << page
+
           end
-
-        # Other pages
-        else
-
-          @otherPages << page
-
+          
         end
-        
       end
 
       # puts @companies.to_yaml
