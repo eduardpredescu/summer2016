c1dea0c721ed28cb9ea1bc46d8cbe757a6a4717f
Valentin Popescu <ppsc.valentin@gmail.com>
2016-05-13 18:00:40 +0300

    Fixed summerjobs.rb

--- _plugins/summerjobs.rb
+++ _plugins/summerjobs.rb
@@ -8,6 +8,14 @@ module Summerjobs
       @jobs = {}
       @otherPages = []
 
+      companies = site.pages.select do |page|
+        page.data['company']
+      end
+
+      jobs = site.pages.select do |page|
+        page.data['job']
+      end
+
       def slug(string)
         string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
       end
@@ -17,66 +25,48 @@ module Summerjobs
         @companies[ companyId ]
       end
 
-      site.pages.each do |page|
+      companies.each do |page|
+        if not page.data['demo']
+          company = page.data['company']
+          company['id'] = slug(company['name'])
+          company['jobs'] = {}
+          company['page'] = page
 
-        # dateCreated = File.ctime(page['path']).iso8601
-        # dateModified = File.mtime(page['path']).iso8601
-        # page.data['dateCreated'] = dateCreated
-        # page.data['dateModified'] = dateModified
-
-        if page.data
-          
-          # Companies
-          if page.data['company']
-            if not page.data['demo']
-              company = page.data['company']
-              company['id'] = slug(company['name'])
-              company['jobs'] = {}
-              company['page'] = page
-
-              page.data.merge!({
-                'layout' => 'company',
-                'company' => company
-              })
-
-              @companies[company['id']] = company
-            else
-              page.data['layout'] = 'company'  
-            end
-
-          # Jobs
-          elsif page.data['job']
-            if not page.data['demo']
-            
-              job = page.data['job']
-              job['company'] = (company = getCompany(page))
-              job['id'] = company['id'] + '_' + slug(job['title'])
-              job['page'] = page
-
-              company['jobs'][job['id']] = job
-
-              page.data.merge!({
-                'layout' => 'job',
-                'job' => job,
-                'company' => company
-              })
-              @jobs[job['id']] = job
-            else
-              page.data['layout'] = 'job'
-            end
-
-          # Other pages
-          else
-
-            @otherPages << page
-
-          end
-          
+          page.data.merge!({
+            'layout' => 'company',
+            'company' => company
+          })
+          @companies[company['id']] = company
+        else
+          page.data['layout'] = 'company'  
         end
       end
 
-      # puts @companies.to_yaml
-      # puts @jobs.to_yaml
+      jobs.each do |page|
+        if not page.data['demo']
+        
+          job = page.data['job']
+          job['company'] = (company = getCompany(page))
+          job['id'] = company['id'] + '_' + slug(job['title'])
+          job['page'] = page
+
+          company['jobs'].merge!({
+            job['id'] => job
+          })
+
+          page.data.merge!({
+            'layout' => 'job',
+            'job' => job,
+            'company' => company
+          })
+          @jobs[job['id']] = job
+        else
+          page.data['layout'] = 'job'
+        end
+      end
+
+      puts @companies.keys
+      puts @jobs.keys
 
       site.data['companies'] = @companies
       site.data['jobs'] = @jobs
