c88b001adbf8e59f5aa66094323b09d7df8619a3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2016-05-23 15:28:37 +0300

    refactored map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,92 +1,96 @@
-var map;
-var geocoder;
-var address;
-var directionsDisplay;
-var mapCanvas;
+
 
 function loadScript() {
-   var script = document.createElement('script');
+  var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js' +
       '?key=AIzaSyCgzwlVgB2yRIIW89qbpi9dAiWlD1DAjy0' + '&libraries=places'+
-      '&callback=summermap.initialize';
+      '&callback=summer.init';
   document.body.appendChild(script);
 }
 
-summermap = {};
-
-summermap.initialize = function() {
-  mapCanvas = document.getElementById('map-canvas');
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  address = mapCanvas.getAttribute('data-address');
-  mapper();
-
-}
-
-
-function mapper(){
-  if (!mapCanvas) return;
-  var start = {};
-  navigator.geolocation.watchPosition(showPosition, showError);
-  function showPosition(position) {
-    start = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    var directionsService = new google.maps.DirectionsService();
-    var request = {
-      origin: start,
-      destination: address,
-      travelMode: google.maps.TravelMode.TRANSIT
-    };
-    directionsService.route(request, function (result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(result);
-      }
-    });
-    map = new google.maps.Map(mapCanvas, {
-      zoom: 17,
-      scrollwheel: false,
-      center: start,
-      styles: []
-    });
-    directionsDisplay.setMap(map);
-  }
-
-  function showError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        console.warn("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        console.warn("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        console.warn("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        console.warn("An unknown error occurred.");
-        break;
+if (window.summer === undefined)
+  var summer = {};
+if(summer.dom===undefined)
+  summer.dom={};
+if (summer.plugins === undefined)
+  summer.plugins = {};
+(function () {
+  var googleMaps =function() {
+    var start = {};
+    navigator.geolocation.watchPosition(showPosition, showError);
+    function showPosition(position) {
+      start = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var directionsService = new google.maps.DirectionsService();
+      var request = {
+        origin: start,
+        destination: summer.plugins['google-maps'].address,
+        travelMode: google.maps.TravelMode.TRANSIT
+      };
+      directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          summer.plugins['google-maps'].directionsDisplay.setDirections(result);
+        }
+      });
+      summer.plugins['google-maps'].map = new google.maps.Map(summer.dom.mapCanvas, {
+        zoom: 17,
+        scrollwheel: false,
+        center: start,
+        styles: []
+      });
+      summer.plugins['google-maps'].directionsDisplay.setMap(summer.plugins['google-maps'].map);
     }
-    map = new google.maps.Map(mapCanvas, {
-      zoom: 17,
-      scrollwheel: false,
-      center: codeAddress(address),
-      styles: []
-    });
 
-  }
+    function showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          console.warn("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.warn("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          console.warn("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERR:
+          console.warn("An unknown error occurred.");
+          break;
+      }
+      summer.plugins['google-maps'].map = new google.maps.Map(summer.dom.mapCanvas, {
+        zoom: 17,
+        scrollwheel: false,
+        center: codeAddress(summer.plugins['google-maps'].address),
+        styles: []
+      });
+
+    }
 
 
+  };
+
+  summer.plugins['google-maps'] = googleMaps;
+}());
+
+summer.init = function() {
+  summer.dom.mapCanvas = document.getElementById('map-canvas');
+  summer.plugins['google-maps'].directionsDisplay = new google.maps.DirectionsRenderer();
+  summer.plugins['google-maps'].address = summer.dom.mapCanvas.getAttribute('data-address');
+  summer.plugins['google-maps']();
 }
-function codeAddress(address) {
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': address}, function(results, status) {
+
+
+
+summer.plugins['google-maps'].codeAddress=function codeAddress(address) {
+  summer.plugins['google-maps'].geocoder = new google.maps.Geocoder();
+  summer.plugins['google-maps'].geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+        map: summer.plugins['google-maps'].map,
+        position: results[0].geometry.location
       });
     } else {
       alert("Geocode was not successful for the following reason: " + status);
